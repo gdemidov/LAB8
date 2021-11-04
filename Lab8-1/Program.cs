58bb54019b5ddc980d6079d240a0e79a4fcdba83
demidov_g@mail.ru <demidov_g@mail.ru>
2021-11-04 15:22:43 +0300

    22

--- Lab8-1/Program.cs
+++ Lab8-1/Program.cs
@@ -18,15 +18,36 @@ namespace Lab8_1
                 directory.Create();
             }
             string filepath = "rd.txt";
-            File.Create(path+filepath);
-            if (!File.Exists(path + filepath))
+            //File.Create(path+filepath);
+
+            // создание массива случайных чисел
+            const int n = 10;
+            int[] array = new int[n];
+            string str = "";
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
             {
-                File.Create(path + filepath);
+                array[i] = random.Next(0, 10);
+                Console.Write("{0} ", array[i]);// для проверки выводим на консоль
+                str += array[i]+" "; //превращаем в строку разделитель  пробел
             }
-            Console.WriteLine("файл создан {0}", (path + filepath));
-            StreamWriter sw = new StreamWriter((path + filepath),true);
-            sw.WriteLine("11");
-            sw.Close();
+            Console.WriteLine();
+
+            //записываем в файл
+            using (StreamWriter sw = new StreamWriter((path + filepath), false))
+            {
+                sw.WriteLine(str);
+            }
+            //считываем из файла
+            using (StreamReader sr = new StreamReader(path + filepath))
+            {
+                str = sr.ReadLine();
+            }
+            
+
+            //выводим на экран
+            
+            Console.Write("{0} ", str);
             Console.ReadKey();
         }
     }
